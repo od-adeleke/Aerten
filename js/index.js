224cb25f1c37e3079c4c65caf9cfa9102b54984d
David Adeleke <90906886+od-adeleke@users.noreply.github.com>
2024-05-23 21:25:10 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        image: './assets/tanner.png',
+        name: 'Tanner Finsha',
+        id: '#23454GH6J7YT6',
+        roleTitle: 'Product Designer',
+        roleContract: 'Full Time',
+        status: 'Active',
+        Teams: 'Marketing, Design, +1'
+    }
+]
+
+const body = document.querySelector('.body-content')
